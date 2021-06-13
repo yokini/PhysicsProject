ef34e02dc4871a04139198937ff6d36b8490c19a
yokini <56157066+yokini@users.noreply.github.com>
2021-06-13 15:37:13 +0400

    Water, Joint and Ragdoll
    Water scene completed
    Joint scene completed and cleaned up
    Ragdoll Scene set up

---
+++ PhysicsAssignment/Assets/Scripts/CharacterControl.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterControl : MonoBehaviour
+{
+    //variables
+    public Rigidbody rb;
+    public float movespeed;
+    private Vector3 moveDir;
+
+    
+    void Start()
+    {
+        // initialization
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        //inputs for movement
+        moveDir = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
+    }
+
+    private void FixedUpdate()
+    {
+        //movement usinf Rigidbody.MovePosition
+        rb.MovePosition(rb.position + transform.TransformDirection(moveDir) * movespeed * Time.deltaTime);
+
+       
+    }
+}
---
+++ PhysicsAssignment/Assets/Scripts/FauxGravityBody.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FauxGravityBody : MonoBehaviour
+{
+    //variables
+    public Rigidbody rb;
+
+    //Reference to the FauxGravityAttractor Script
+    public FauxGravityAttractor attractor;
+
+    void Start()
+    {
+        //initialization
+        rb = GetComponent<Rigidbody>();
+    }
+
+   
+    void Update()
+    {
+        //Have the body attracted to the planet, attract method from the FauxGravityAttractor Script
+        attractor.Attract(transform, rb);
+    }
+}
